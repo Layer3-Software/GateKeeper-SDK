3730887f87ded544a4957b43d89fe94162dd83d1
Nico Limo <nicolas.limonoff@gmail.com>
2022-09-27 16:23:09 -0300

    first commit

---
+++ src/LayerModal.d.ts
@@ -0,0 +1,3 @@
+export interface LayerModalProps {
+  account: string;
+}
---
+++ src/LayerModal.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { LayerModalProps } from './LayerModal.d';
+
+export const LayerModal = ({ account }: LayerModalProps) => {
+  return <div>LayerModal {account}</div>;
+};
