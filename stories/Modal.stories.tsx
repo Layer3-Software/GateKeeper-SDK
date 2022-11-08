ab9283243e4eabe767aee4f97a7b9a61c8eb6fb0
Nico Limo <nicolas.limonoff@gmail.com>
2022-11-08 14:36:04 -0300

    fix(size): height size

--- stories/Modal.stories.tsx
+++ stories/Modal.stories.tsx
@@ -8,7 +8,7 @@ export default {
 } as ComponentMeta<typeof Modal>;
 
 const Template: ComponentStory<typeof Modal> = () => {
-  const accountNotVerified = '0xAA03727c738c60cc45887563e4986D0466DB5972';
+  const accountNotVerified = '0xAc4b0639A5b6509134fB3AB37A2BBc410974a0eb';
 
   return <Modal account={accountNotVerified} />;
 };
