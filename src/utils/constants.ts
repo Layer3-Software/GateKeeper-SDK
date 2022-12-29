741dc4e9c8f113582f128d754510299ca877a649
Charlie <13856015+KyrneDev@users.noreply.github.com>
2022-12-29 12:32:03 -0800

    Update website url to black-smoke

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,7 +5,7 @@ export const MODAL_NOT_ALLOWED = {
     'Your location does not allow access without successful verification. Verify yourself and your wallet now with our KYC partner GateKeeper.',
 };
 
-export const WEBSITE = 'https://lively-sky-0a9d8bd1e.1.azurestaticapps.net';
+export const WEBSITE = 'https://black-smoke-0ba344710.2.azurestaticapps.net';
 
 export const DEFAULT_COLORS = {
   primaryColor: '#059669',
