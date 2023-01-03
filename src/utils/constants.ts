13bbaf8d2a30e2674e5546356eaf727adf2e94f4
Nicolas carballo <nlucas120@gmail.com>
2023-01-03 17:44:12 -0300

    fix: adding variable to constant file

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,8 @@
 export const BACKEND_URL = 'https://kyc-backend-api.azurewebsites.net/v1/';
 export const WEBSITE = 'https://black-smoke-0ba344710.2.azurestaticapps.net';
 
+export const GEO_API_KEY = '8dd79c70-0801-11ec-a29f-e381a788c2c0';
+
 export const DEFAULT_COLORS = {
   primaryColor: '#059669',
   textColor: '#e2e8f0',
