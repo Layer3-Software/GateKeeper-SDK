650a6e6cc00743e05daa83e495bc692c497d9028
Nicolas carballo <nlucas120@gmail.com>
2023-04-17 15:02:28 -0300

    fix: updating env variable to control calls endpoint

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,8 +1,5 @@
-const isProduction = process.env.GATEKEEPER_PRODUCTION === 'true';
-
-export const BACKEND_URL = isProduction
-  ? 'https://api.gatekeeper.software/v1'
-  : 'https://gk-prod-backend-staging.azurewebsites.net/v1';
+export const BACKEND_URL =
+  'https://gk-prod-backend-staging.azurewebsites.net/v1';
 
 export const WEBSITE = 'https://black-smoke-0ba344710.2.azurestaticapps.net';
 export const GEO_API_KEY = '8dd79c70-0801-11ec-a29f-e381a788c2c0';
