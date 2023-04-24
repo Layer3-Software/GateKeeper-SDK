fc963134ecc4289861eaf533ab117e1c1427c7b5
KyrneDev <charlie.kern01@gmail.com>
2023-04-23 17:15:50 -0700

    fix(sign message): updated sign message for login/register

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,6 +2,9 @@ export const PRODUCTION_BACKEND_URL = 'https://api.gatekeeper.software/v1';
 export const STAGING_BACKEND_URL =
   'https://gk-prod-backend-staging.azurewebsites.net/v1';
 
+export const SIGN_MESSAGE =
+  'Only sign if you are trying to login, or verify your credentials on GateKeeper.';
+
 export const WEBSITE = 'https://black-smoke-0ba344710.2.azurestaticapps.net';
 export const GEO_API_KEY = '8dd79c70-0801-11ec-a29f-e381a788c2c0';
 
@@ -11,8 +14,6 @@ export const DEFAULT_COLORS = {
   buttonTextColor: '#e2e8f0',
   backgroundColor: '#141724',
 };
-
-export const SIGN_MESSAGE = 'Only sign if you are trying to login, or verify your credentials on GateKeeper.';
 
 export const ONE_CHECK_ERROR = 'Specify at least one check';
 export const POLYGON_DID_ERROR = 'This role requires a Polygon DID';
