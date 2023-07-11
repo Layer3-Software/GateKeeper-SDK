b93bb7cc4ef5f1abfb2e4a26cf55657b1fe1d683
Nicolas carballo <nlucas120@gmail.com>
2023-07-11 11:53:50 -0300

    fix: adding new condition to getSignature function

--- src/utils/getSignature.ts
+++ src/utils/getSignature.ts
@@ -4,26 +4,29 @@ export const SIGN_MESSAGE =
   "Only sign if you are trying to login, or verify your credentials on GateKeeper.";
 
 export interface SignatureResponse {
-  signature?: string;
+  signature: string;
   error?: string;
 }
 
 const getSignature = async (
   address: string,
   signer: any,
-  appId: string,
+  appId?: string,
+  allowUserRegistration: boolean = true,
   isStaging: boolean = false,
 ): Promise<SignatureResponse> => {
-  if (!address) return { error: "No address provided" };
-  if (!signer) return { error: "No signer provided" };
-  if (!appId) return { error: "No appId provided" };
+  if (!address) return { error: "No address provided", signature: "" };
+  if (!signer) return { error: "No signer provided", signature: "" };
 
   const { nonce } = await getNonce(address, isStaging);
 
-  if (nonce === undefined || nonce === null) {
-    const signature = await signer.signMessage(SIGN_MESSAGE);
-    await register(address, signature, appId, isStaging);
-    return { signature };
+  if (allowUserRegistration) {
+    if (!appId) return { error: "No appId provided", signature: "" };
+    if (nonce === undefined || nonce === null) {
+      const signature = await signer.signMessage(SIGN_MESSAGE);
+      await register(address, signature, appId, isStaging);
+      return { signature };
+    }
   }
 
   const signature = await signer.signMessage(`${SIGN_MESSAGE} Nonce: ${nonce}`);
